7fec9f1e5f78743930ea4374c7fa301267dc10ef
Arina Bukhonova <arinabukhonova@gmail.com>
2021-11-24 15:17:52 +0300

    Int

---
+++ app.py
@@ -0,0 +1,22 @@
+from translate import Translator
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def trans():
+    if request.method == 'POST':
+        translator = Translator(to_lang='ru')
+        result = request.form['text_for_trans']
+        result = translator.translate(result)
+        return render_template("index.html", text_after_trans=result)
+    if request.method == 'GET':
+        return render_template('index.html')
+
+
+if __name__ == '__main__':
+ app.run()
+
+
+
+
